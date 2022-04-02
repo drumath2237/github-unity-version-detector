1d2921fe82b8662ac19efa405d9060c720d95bcf
drumath <2237k.drumath@gmail.com>
2022-04-03 01:37:49 +0900

    :package: chrome typesとタブ情報の取得

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,23 @@ import { GitHubUnityVersionDetector } from "./GitHubUnityVersionDetector";
 import "./style.scss";
 
 window.addEventListener("load", () => {
+  const headerTitle = <HTMLSpanElement>document.getElementById("header-title");
+
+  if (!headerTitle) {
+    return;
+  }
+
+  chrome.tabs.query(
+    {
+      active: true,
+      currentWindow: true,
+    },
+    (tabs) => {
+      const url = tabs[0].url;
+      headerTitle.innerText = url ?? "";
+    }
+  );
+
   const detector = new GitHubUnityVersionDetector();
   App.main(detector);
 });
