c3328b2ecb5e91f211953b238c9538a249352122
drumath <2237k.drumath@gmail.com>
2022-04-03 00:59:17 +0900

    :tada: リポジトリ情報からバージョン情報の取得に成功

--- src/GitHubUnityVersionDetector.ts
+++ src/GitHubUnityVersionDetector.ts
@@ -7,10 +7,33 @@ class GitHubUnityVersionDetector implements IVersionDetector {
     branch: string,
     path?: string
   ): Promise<detectionResult> {
-    // TODO: impl
+    const rawTextPath = `https://raw.githubusercontent.com/${username}/${repo}/${branch}${
+      path ? `/${path}` : ""
+    }/ProjectSettings/ProjectVersion.txt`;
+
+    const rawRequestResult = await fetch(rawTextPath, { method: "GET" });
+    if (!rawRequestResult.ok) {
+      return {
+        result: false,
+        version: "",
+      };
+    }
+
+    const resultText = await rawRequestResult.text();
+
+    const versionRegexp = /m_EditorVersion: ((\d|\.)+.\d+)/;
+    const m = versionRegexp.exec(resultText);
+
+    if (!m || m[1] === null) {
+      return {
+        result: false,
+        version: "",
+      };
+    }
+
     return {
       result: true,
-      version: "aa",
+      version: m[1],
     };
   }
 }
